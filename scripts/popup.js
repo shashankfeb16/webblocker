576e2a1269643650b30797b747ced4fbdeb37f9c
shashankfeb16 <shashankfeb16@gmail.com>
2022-09-02 21:23:05 +0530

    bugs fixed

--- scripts/popup.js
+++ scripts/popup.js
@@ -39,13 +39,13 @@ document.getElementById("btn").addEventListener("click",()=>{
                 });
 
                 setTimeout(()=>{
-                    var blk = new Date();
-                    blk.setHours(1,0,0,0);
-                    const blkTill = blk.getTime();
+                    var then = new Date();
+                    then.setHours(24,0,0,0);
+                    const blockTill = then.getTime();
 
                     chrome.storage.local.set({
                         BlockedUrls:[{
-                            status: "Blocked", url: Hostname, blk: blkTill
+                            status: "BLOCKED", url: Hostname, BlockTill: blockTill
                         }]
                     },5000)
                 })
@@ -55,7 +55,7 @@ document.getElementById("btn").addEventListener("click",()=>{
                     ErrorShown("This Url will blocked after some time");
 
                 }
-                else if(data.BlockedUrls.some((e)=>e.url===Hostname && e.status==="Blocked")){
+                else if(data.BlockedUrls.some((e)=>e.url===Hostname && e.status==="BLOCKED")){
                     ErrorShown("This URL is blocked ")
                 }
                 else{
@@ -72,11 +72,11 @@ document.getElementById("btn").addEventListener("click",()=>{
                                 if(e.url===Hostname && e.status==="In_Progress"){
                                     var arr = data.BlockedUrls.splice(index,1);
 
-                                    var blk = new Date();
-                                    blk.setHours(1,0,0,0);
-                                    const blkTill = blk.getTime();
+                                    var then = new Date();
+                                    then.setHours(24, 0, 0, 0);
+                                    const blockTill = then.getTime()
 
-                                    chrome.storage.local.set({BlockedUrls:[...arr,{ status: "Blocked", url: Hostname, blk: blkTill}]})
+                                    chrome.storage.local.set({BlockedUrls:[...arr,{ status: "BLOCKED", url: Hostname, BlockTill: blockTill}]})
 
                                 }
                             })
